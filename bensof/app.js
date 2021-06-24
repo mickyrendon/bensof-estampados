40d9b7ebbfe291069939861769266666629f00d9
micky rendon <elderendon@hotmail.es>
2021-06-24 01:30:10 -0300

    features menu mobile

--- bensof/app.js
+++ bensof/app.js
@@ -5,3 +5,35 @@ const element1   = document.querySelector('.secciones');
 function down(e){
 
 }
+
+// menu mobiless/hacer que suba
+const menuBtn = document.querySelector('.menu');
+const menuLista = document.querySelector('.ul-list');
+      menuLista.style.display = 'none';
+
+
+let pushDown = menuBtn.addEventListener('click', dropdown, true);
+let pushUp   = menuBtn.addEventListener('click', dropup, true);  
+
+function dropdown(e){
+    e.target.style.background = 'red';
+    menuLista.style.display = 'block'
+    console.log('target');
+    menuBtn.removeEventListener('click', dropdown, false);
+
+}
+function dropup(e){
+    e.target.style.background = 'blue';
+    menuLista.style.display = 'none'
+    console.log('target up');
+    menuBtn.removeEventListener('click', dropup, false);
+
+}
+
+// if(!checked){
+//     e.target.style.background = 'blue';
+//     menuLista.style.display = 'none';
+//     console.log('if de target');
+// }else{
+//      checked
+// }
