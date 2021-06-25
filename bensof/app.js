fcb01476f500f250dc1f91ea7d34edce1096bb92
micky rendon <elderendon@hotmail.es>
2021-06-25 01:41:26 -0300

    menu mobile y boton ver mas en js funcionando!

--- bensof/app.js
+++ bensof/app.js
@@ -1,39 +1,58 @@
+// ver mas ver menos x
 const seeMoreBtn = document.querySelector('.see-more');
-const element1   = document.querySelector('.secciones');
+      seeMoreBtn.style.backgroundImage = 'url(/assets/arrowDown.png)';        seeMoreBtn.style.backgroundRepeat = 'no-repeat';
 
-// ver mas ver menos 
+const seeMoreSections = document.querySelector('.see-more-section');
+      seeMoreSections.style.width = '100%';
+      seeMoreSections.style.display = 'none';
+
+seeMoreBtn.addEventListener('click', down, true);
+
+// agregar efectos agradables al plegar y desplegar.... ease-in etc
 function down(e){
+    let status = seeMoreSections.style.display;
+    console.log('clickeando boton ver más');
 
+    if (status == 'none'){
+        
+        console.log('seccion desplegada');
+        e.target.style.backgroundImage = 'url(/assets/arrowUp.png)';
+        seeMoreSections.style.display = 'block';
+
+    }else{
+        
+        console.log('seccion plegada');
+        e.target.style.backgroundImage = 'url(/assets/arrowDown.png)';
+        seeMoreSections.style.display = 'none';
+
+    };
 }
 
-// menu mobiless/hacer que suba
+// menu mobile
 const menuBtn = document.querySelector('.menu');
+      menuBtn.style.background = '#ffff'
 const menuLista = document.querySelector('.ul-list');
       menuLista.style.display = 'none';
 
-
-let pushDown = menuBtn.addEventListener('click', dropdown, true);
-let pushUp   = menuBtn.addEventListener('click', dropup, true);  
+// evento
+menuBtn.addEventListener('click', dropdown, true);
 
 function dropdown(e){
-    e.target.style.background = 'red';
-    menuLista.style.display = 'block'
-    console.log('target');
-    menuBtn.removeEventListener('click', dropdown, false);
+    let status = menuLista.style.display;
+    console.log('clickeando menu mobile');
 
-}
-function dropup(e){
-    e.target.style.background = 'blue';
-    menuLista.style.display = 'none'
-    console.log('target up');
-    menuBtn.removeEventListener('click', dropup, false);
+    if(status == 'none'){
+    
+        console.log('menu desplegado');
+        e.target.style.background = 'red';
+        menuLista.style.display = 'block';
 
-}
+    }else{
+        
+        console.log('menu plegado');
+        e.target.style.background = '#ffff';
+        menuLista.style.display = 'none';
 
-// if(!checked){
-//     e.target.style.background = 'blue';
-//     menuLista.style.display = 'none';
-//     console.log('if de target');
-// }else{
-//      checked
-// }
+    };
+
+};
